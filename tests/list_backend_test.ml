59cb1b1a81fa3bb8ec050d64863ee78560b7fa61
Malcolm Matalka <malcolm.matalka@klarna.com>
2014-06-10 23:12:37 +0200

    Update build system and chance to gen_server implementation

--- tests/list_backend_test.ml
+++ tests/list_backend_test.ml
@@ -4,14 +4,14 @@ open Async.Std
 let main () =
   let backend = Regen_event_list_backend.create () in
   let handler = Regen_event_list_backend.handler backend in
-  let server = Regen_event.start () in
-  Regen_event.add_handler server handler >>= fun _ ->
-  Regen_event.publish server "blah"      >>= fun () ->
+  Regen_event.start ()                   >>= fun server->
+  Regen_event.add_handler server handler >>=? fun _ ->
+  Regen_event.publish server "blah"      >>=? fun () ->
   Regen_event.sync server                >>= fun () ->
-  Regen_event.stop server;
+  Regen_event.stop server                >>=? fun () ->
   if Regen_event_list_backend.to_list backend <> ["blah"] then
     failwith "Failed!";
-  Deferred.return (shutdown 0)
+  Deferred.return (Ok (shutdown 0))
 
 let () =
   ignore (main ());
